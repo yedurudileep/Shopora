76e1096d7e97d3ff163e7069e4d84f459ff62a72
yedurudileep <yedurudileep771981@gmail.com>
2026-06-21 15:48:39 +0530

    Update production API URL

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,6 +1,17 @@
-const Razorpay=require("razorpay")
-const razorpay=new Razorpay({
-    key_id: process.env.RAZORPAY_KEY_ID,
-    key_secret: process.env.RAZORPAY_KEY_SECRET,
+const Razorpay = require("razorpay");
+
+if (
+  !process.env.RAZORPAY_KEY_ID ||
+  !process.env.RAZORPAY_KEY_SECRET
+) {
+  throw new Error(
+    "RAZORPAY_KEY_ID or RAZORPAY_KEY_SECRET is missing"
+  );
+}
+
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
-module.exports=razorpay;
+
+module.exports = razorpay;
